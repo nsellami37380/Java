10aa7032cc541a3bdcfbd000dcb033b15b8d08b6
nsellami37380 <82585130+nsellami37380@users.noreply.github.com>
2022-10-24 10:38:49 +0200

    IndianaJones

---
+++ IndianaJones.java
@@ -0,0 +1,15 @@
+import javax.xml.crypto.Data;
+
+class IndianaJones{
+    public static void main(String[] args) {
+        String originalTitle = "Indiana Jones and the Last Crusad";
+        Boolean isViewed = false;
+        int yearReleased = 1989;
+        int fourthNote = 7;
+
+        System.out.println(originalTitle + "\n" +
+                            isViewed + "\n" +
+                            yearReleased+ "\n" + 
+                            fourthNote );
+    }
+}
